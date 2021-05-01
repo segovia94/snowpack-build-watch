5c5954b5e330863dc777bfb4aacefed79ce7865d
Mark Miller <mark@zhukmedia.com>
2021-04-30 23:38:21 -0700

    no index.html entry causes no hmr files

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,27 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
+    // public: { url: '/', static: true },
     src: { url: '/dist' },
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+  }
 };
